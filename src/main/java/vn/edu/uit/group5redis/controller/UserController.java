a8011f2c73f78bafef995a8b5460527cf955e09c
Truong Vu <vunhtruong@gmail.com>
2022-08-24 17:09:47 +0700

    Update Redis Group 5 Config (#2)
    Co-authored-by: truongvu <truong.vun1@homecredit.vn>

--- src/main/java/vn/edu/uit/group5redis/controller/UserController.java
+++ src/main/java/vn/edu/uit/group5redis/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import vn.edu.uit.group5redis.service.UserService;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @GetMapping("/{uuid}")
